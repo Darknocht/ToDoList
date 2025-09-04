d055f48a5c84653d3c1fd5254fb3d2f409413fd8
Darknocht <darknocht@gmail.com>
2025-09-04 13:23:57 +0200

    Update server.js with commands GET and POST, comments for functions class readingWritingDatabase, add an example in tasks.json

--- api/server.js
+++ api/server.js
@@ -20,6 +20,43 @@ app.get('/', (req, res) => {
     res.send('Hello World!\n ' + rwDB.readTasks())
 })
 
+//Different commands for a task
+
+//GET /tasks
+app.get('/tasks', (req, res) => {
+    res.json(rwDB.readTasks())
+})
+
+//POST /tasks
+app.post('/tasks', (req, res) => {
+    const {title, description="", status = "todo" } = req.body;
+
+    //Checking arguments
+    //title must not empty and max 100 characters
+    if(title.length <= 0 || title.length > 100){
+        return res.status(400).json({ error: "Invalid title" });
+    }
+    //description must max 500 characters and not contain <script>
+    if(description.length > 500 || /script/i.test(description)){
+        //<script> can be <SCRIPT>, <ScRiPt>, etc.
+        return res.status(400).json({ error: "Invalid description" });
+    }
+    //status must contain todo, in-progress or done
+    if(!["todo","in-progress","done"].includes(status)){
+        //We can use includes because we have only 3 values
+        return res.status(400).json({ error: "Invalid status" });
+    }
+
+    //After Checking, the task will be added
+    const tasks = rwDB.readTasks();
+    const newTask = { id: Date.now(), title, description, status };
+    tasks.push(newTask);
+    rwDB.writeTasks(tasks);
+
+    //Code 201 = new created task without problem
+    res.status(201).json(newTask);
+})
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
--- api/src/readingWritingDatabase.js
+++ api/src/readingWritingDatabase.js
@@ -2,17 +2,29 @@ const fs = require("fs");
 
 class readingWritingDatabase {
 
-    //Private element
-    #DATA_FILE;
+    #DATA_FILE; //Private element
     constructor(DATA_FILE) {
         this.DATA_FILE = DATA_FILE;
     }
 
+    /**
+     * Read a task in DATA_FILE with his different components (title, description, status)
+     * @returns {any|*[]}
+     */
     readTasks() {
-        if (!fs.existsSync(this.DATA_FILE)) return [];
-        return JSON.parse(fs.readFileSync(this.DATA_FILE));
+        //Data_FILE does not exist
+        if (!fs.existsSync(this.DATA_FILE)){
+            return [];
+        }
+        else{
+            return JSON.parse(fs.readFileSync(this.DATA_FILE));
+        }
     }
 
+    /**
+     * Write a new task in the file DATA_FILE
+     * @param {Object} tasks with his different components (title, description, status)
+     */
     writeTasks(tasks) {
         fs.writeFileSync(this.DATA_FILE, JSON.stringify(tasks, null, 2));
     }
