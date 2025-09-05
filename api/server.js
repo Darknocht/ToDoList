a6328b1577a24188bd11214bc7ed62564cc3491e
Darknocht <darknocht@gmail.com>
2025-09-05 12:15:11 +0200

    Add PATCH and DELETE in server.js

--- api/server.js
+++ api/server.js
@@ -24,8 +24,8 @@ app.get('/', (req, res) => {
 
 //GET /tasks
 app.get('/tasks', (req, res) => {
-    res.json(rwDB.readTasks())
-})
+    res.status(200).json(rwDB.readTasks()) //Code 200 (OK)
+});
 
 //POST /tasks
 app.post('/tasks', (req, res) => {
@@ -55,7 +55,53 @@ app.post('/tasks', (req, res) => {
 
     //Code 201 = new created task without problem
     res.status(201).json(newTask);
-})
+});
+
+//PATCH /tasks:id
+app.patch('/tasks/:id', (req, res) => {
+    //Given task
+    const {id} = req.params;
+    const {status} = req.body;
+
+    //All tasks stocks
+    const allTasks = rwDB.readTasks();
+
+    //Checking if we find the task in allTasks
+    const task = allTasks.find(task => task.id === id);
+    if(!task){
+        return res.status(404).json({ error: "Task not found" });
+    }
+
+    //Checking if the status is between the 3 values
+    if(!["todo", "in-progress", "done"].includes(status)){
+        return res.status(400).json({ error: "Invalid status" });
+    }
+
+    //Update status of task in DATA_FILE
+    task.status = status;
+    rwDB.writeTasks(allTasks);
+    res.status(200).json(task); //Code 200 (OK)
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    //id for the deleted task
+    const {id} = req.params;
+
+    //All tasks stocks
+    const allTasks = rwDB.readTasks();
+
+    //We copy the all tasks in newTasks except the deleted task
+    const newTasks = allTasks.filter(t => t.id !== id);
+
+    //If the length of both are equals, the task wasn't deleted
+    if(allTasks.length === newTasks.length){
+        return res.status(404).json({ error: "Task not found" });
+    }
+
+    //Update the tasks in DATA_FILE
+    rwDB.writeTasks(newTasks);
+    res.status(204).send(); //Code 204 (OK, but no additional content)
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
