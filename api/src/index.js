231b8671e7f7b3e3351812a63ca0b31c50e091ff
Darknocht <darknocht@gmail.com>
2025-09-22 12:23:48 +0200

    Update index.js with PORT

--- api/src/index.js
+++ api/src/index.js
@@ -1,2 +1,3 @@
 const app = require("./server");
-app.listen(3000, () => console.log("API running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("API running on http://localhost:3000"));
