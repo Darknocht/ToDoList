eeb51e1332adc052bc3d45a70128608db6540a60
Darknocht <darknocht@gmail.com>
2025-10-06 16:13:33 +0200

    Delete api/__mocks__ and dompurify.js

--- api/__mocks__/dompurify.js
+++
@@ -1,7 +0,0 @@
-/*module.exports = function createDOMPurify() {
-    //Avoid crashing for GitHub Actions
-    console.log("DOMPurify mock loaded");
-    return {
-        sanitize: (input) => input.replace(/<script.*?>.*?<\/script>/gi, '')
-    };
-};*/
