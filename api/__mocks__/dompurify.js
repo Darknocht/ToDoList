c0d15d0564f3ae7155fff325219d987254582dfa
Darknocht <darknocht@gmail.com>
2025-10-06 16:11:46 +0200

    Test to disable api/__mocks__/dompurify.js

--- api/__mocks__/dompurify.js
+++ api/__mocks__/dompurify.js
@@ -1,7 +1,7 @@
-module.exports = function createDOMPurify() {
+/*module.exports = function createDOMPurify() {
     //Avoid crashing for GitHub Actions
     console.log("DOMPurify mock loaded");
     return {
         sanitize: (input) => input.replace(/<script.*?>.*?<\/script>/gi, '')
     };
-};
+};*/
